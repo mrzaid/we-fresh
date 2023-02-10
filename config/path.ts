6af839d47e7f57892d49f67293870cb2828f9b85
mrzaid <zaidahmed87861@gmail.com>
2023-02-10 10:14:23 -0800

    changes

--- config/path.ts
+++ config/path.ts
@@ -1,6 +1,8 @@
+// class Paths {
+//     static CONTENTFUL_URI = `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}/`
+//   }
+
 class Paths {
-    static CONTENTFUL_URI = `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}/`
-  }
-  
-  
-  export default Paths
+  static CONTENTFUL_URI = `https://graphql.contentful.com/content/v1/spaces/fr6kcovl026y/`;
+}
+export default Paths;
--- handlers/index.ts
+++ handlers/index.ts
@@ -1,22 +1,25 @@
-import Paths from '../config/path'
+import Paths from "../config/path";
 export const fetchContent = async (query: string) => {
   try {
-    const res = await fetch(Paths.CONTENTFUL_URI, {
-      method: 'POST',
-      body: JSON.stringify({ query: query }),
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
-      },
-    })
+    const res = await fetch(
+      "https://graphql.contentful.com/content/v1/spaces/fr6kcovl026y/",
+      {
+        method: "POST",
+        body: JSON.stringify({ query: query }),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer iNlQ1MMJfxj99iiuwVMO9Rz1Uoies5hKeTPFI5rHLQY`,
+        },
+      }
+    );
 
-    const fetchedContent = await res.json()
+    const fetchedContent = await res.json();
 
-    return fetchedContent
+    return fetchedContent;
   } catch (error) {
     // add a descriptive error message first,
     // so we know which GraphQL query caused the issue
 
-    console.error(error)
+    console.error(error);
   }
-}
+};
