45372ed4beed7f8b2ca1396580e0d01724bdde0f
mrzaid <zaidahmed87861@gmail.com>
2023-02-10 10:04:25 -0800

    changes config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
-{
-    env: {
-      CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID;
-      CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN;
-      CONTENTFUL_URI :process.env.CONTENTFUL_URI
-    }
-  }
-  
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
+    CONTENTFUL_URI: process.env.CONTENTFUL_URI,
+  },
+};
+
+module.exports = nextConfig;
